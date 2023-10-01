59efd9b32f54475c8b17c3b2b54b676b2a5ff879
Felipe Jesus <110308764+felipeollveira@users.noreply.github.com>
2023-10-01 10:35:30 -0300

    op update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -40,17 +40,17 @@ class _HomePage extends State<HomePage> {
           'Calculadora de IMC',
           style: TextStyle(
             fontSize: 18,
-            color: Color.fromARGB(26, 226, 226, 226),
+            color: Color.fromARGB(78, 226, 226, 226),
             fontWeight: FontWeight.w300,
           ),
         ),
-        backgroundColor: Colors.black,
+        backgroundColor: Color.fromARGB(12, 138, 138, 138),
         toolbarHeight: 120,
         centerTitle: true,
       ),
-      backgroundColor: Colors.black26,
+      backgroundColor: Color.fromARGB(64, 68, 68, 68),
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+        padding: const EdgeInsets.all(16.0), // Margem em torno de todos os elementos
         child: Center(
           child: AnimatedSwitcher(
             duration: const Duration(milliseconds: 400),
@@ -81,7 +81,6 @@ class IMCCalculator extends StatefulWidget {
   const IMCCalculator({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _IMCCalculatorState createState() => _IMCCalculatorState();
 }
 
@@ -136,40 +135,37 @@ class _IMCCalculatorState extends State<IMCCalculator> {
     } else if (bmi >= 24.91 && bmi < 29.90) {
       return 'Sobrepeso';
     } else {
-      return 'Procure um médico. Nível de IMC elevado.'; // Adicione um valor padrão se necessário
+      return 'Procure um médico. Nível de IMC elevado.';
     }
   }
 
   Color getColor(double bmi) {
     if (bmi == 0) {
-      return Colors.white; // Define a cor padrão
+      return Colors.white;
     } else if (bmi <= 18.40) {
-      return Colors.red; // Define a cor vermelha para IMC menor ou igual a 18.40
+      return Colors.red;
     } else if (bmi < 24.90) {
-      return const Color.fromARGB(255, 61, 170, 65); // Define a cor verde para IMC menor que 24.90
+      return const Color.fromARGB(255, 61, 170, 65);
     } else if (bmi < 29.90) {
-      return Colors.orange; // Define a cor laranja para IMC menor que 29.90
+      return Colors.orange;
     } else {
-      return Colors.red; // Define a cor vermelha para IMC maior ou igual a 29.90
+      return Colors.red;
     }
   }
 
   double calculateBMI() {
-    // Converte altura para metros
     double heightInMeters = height / 100.0;
-    // Calcula o IMC
     double bmi = weight / (heightInMeters * heightInMeters);
     return bmi;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      key: const Key('data'),
-      children: [
-        Container(
-          color: Colors.black,
-          child: Column(
+    return Center(
+      child: Column(
+        key: const Key('data'),
+        children: [
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Text(
@@ -220,213 +216,191 @@ class _IMCCalculatorState extends State<IMCCalculator> {
               ),
             ],
           ),
-        ),
-
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(22, 255, 255, 255),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 14),
-              width: 200,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'PESO',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Color.fromARGB(255, 82, 82, 82),
-                      fontWeight: FontWeight.w400,
-                      height: 0,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        weight.toStringAsFixed(1),
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.w900,
-                        ),
-                      ),
-
-                         const Text(
-                        'kg',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      InkWell(
-                        onTap: aumentarPeso,
-                        child: const Icon(
-                          Icons.add_circle,
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          size: 65,
-                        ),
-                      ),
-                      InkWell(
-                        onTap: diminuirPeso,
-                        child: const Icon(
-                          Icons.remove_circle,
-                          color: Color.fromARGB(255, 214, 214, 214),
-                          size: 65,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(22, 255, 255, 255),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 14),
-              width: 200,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'IDADE',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Color.fromARGB(255, 82, 82, 82),
-                      fontWeight: FontWeight.w400,
-                      height: 0,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        year.toString(),
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.w900,
-                        ),
-                      ),
-                      const Text(
-                        'ys',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      InkWell(
-                        onTap: aumentaIdade,
-                        child: const Icon(
-                          Icons.add_circle,
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          size: 65,
-                        ),
-                      ),
-                      InkWell(
-                        onTap: diminuIdade,
-                        child: const Icon(
-                          Icons.remove_circle,
-                          color: Color.fromARGB(255, 214, 214, 214),
-                          size: 65,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-const SizedBox(
-  height: 16.0, //espaçamento
-),
-            Container(
-          decoration: BoxDecoration(
-          color: const Color.fromARGB(22, 255, 255, 255),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        padding: const EdgeInsets.all(30.0),
-        width:440,
-        child: Center(
-          child: Column(
+    
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              // Exibir o resultado do IMC
-              const Text(
-                'IMC',
-                style: TextStyle(
-                  fontSize: 12.0,
-                  color: Color.fromARGB(48, 255, 255, 255),
-                  fontWeight: FontWeight.bold,
-                  height: 0,
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(22, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 16),
+                width: 175,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'PESO',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Color.fromARGB(255, 82, 82, 82),
+                        fontWeight: FontWeight.w400,
+                        height: 0,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          weight.toStringAsFixed(1),
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+    
+                        const Text(
+                          'kg',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        InkWell(
+                          onTap: aumentarPeso,
+                          child: const Icon(
+                            Icons.add_circle,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            size: 65,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: diminuirPeso,
+                          child: const Icon(
+                            Icons.remove_circle,
+                            color: Color.fromARGB(255, 214, 214, 214),
+                            size: 65,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              Text(
-                calculateBMI().toStringAsFixed(2),
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: getColor(calculateBMI()),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(22, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-              ),
-              Text(
-                classes(calculateBMI()),
-                style: TextStyle(
-                  fontSize: 14,
-                  color: getColor(calculateBMI()),
-                  fontWeight: FontWeight.bold,
-                  height: 0,
+                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 16),
+                width: 175,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'IDADE',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Color.fromARGB(255, 82, 82, 82),
+                        fontWeight: FontWeight.w400,
+                        height: 0,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          year.toString(),
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        const Text(
+                          'ys',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        InkWell(
+                          onTap: aumentaIdade,
+                          child: const Icon(
+                            Icons.add_circle,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            size: 65,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: diminuIdade,
+                          child: const Icon(
+                            Icons.remove_circle,
+                            color: Color.fromARGB(255, 214, 214, 214),
+                            size: 65,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
-        ),
+          const SizedBox(
+            height: 16.0,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(22, 255, 255, 255),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            padding: const EdgeInsets.all(30.0),
+            width: 440,
+            child: Center(
+              child: Column(
+                children: [
+                  const Text(
+                    'IMC',
+                    style: TextStyle(
+                      fontSize: 12.0,
+                      color: Color.fromARGB(48, 255, 255, 255),
+                      fontWeight: FontWeight.bold,
+                      height: 0,
+                    ),
+                  ),
+                  Text(
+                    calculateBMI().toStringAsFixed(2),
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: getColor(calculateBMI()),
+                    ),
+                  ),
+                  Text(
+                    classes(calculateBMI()),
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: getColor(calculateBMI()),
+                      fontWeight: FontWeight.bold,
+                      height: 0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30.0,
+          ),
+          
+         
+        ],
       ),
-      const SizedBox(
-  height: 29.0, // Espaçamento
-  ),
-   Container(
-    
-    child: Center(
-      
-              child: TextButton(
-                onPressed: () {
-                  print('Botão de texto pressionado');
-                },
-                style: TextButton.styleFrom(
-                  foregroundColor: Color.fromARGB(255, 6, 20, 0), 
-                  backgroundColor: Color.fromARGB(212, 248, 248, 248),       
-                  padding: const EdgeInsets.all(16.0),                 
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6.0), 
-                  ),
-                ),
-                child: const Text('Salvar resultado'),
-              )
-                  ),
-                )
-
-      ],
-      
     );
-    
   }
 }
